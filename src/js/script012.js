1d18c51f94dc19e3aa6c6be8d77ad5a679cbd72e
monkepunkk <naden1337@gmail.com>
2022-08-18 21:08:24 +0300

    lesson16

--- src/js/script012.js
+++ src/js/script012.js
@@ -28,12 +28,33 @@ const personalMovieDB = {
     genres :[],
     privat : false
 };
+if (personalMovieDB.count<10 ){
+    alert('маловато');
+} else {
+    if (personalMovieDB.count > 30){
+        alert ('Kinoman');
+    } else {
+        alert ('classic viewer');
+    }
+}
 const a = prompt('Один из последних просмотренных фильмов?',''),    
       b = prompt ('На сколько оцените его?',''),
       c = prompt('Один из последних просмотренных фильмов?',''),
       d = prompt ('На сколько оцените его?','');
 personalMovieDB.movies[a]= b; 
 personalMovieDB.movies[c]= d; 
+for (let i = 0; i < 2;i++) {
+const    a = prompt('Один из последних просмотренных фильмов?',''),    
+    b = prompt ('На сколько оцените его?','');
+    if (a!=null &&  b!=null && a!= '' && b!= '' && a.length < 50){
+        personalMovieDB.movies[a]= b;
+        console.log('done');
+    } else {
+        concole.log('error');
+        i--;
+    }
+}
+console.log(personalMovieDB);
 
 
 
